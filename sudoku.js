5349e11352e21d20acf10efc70e90a3738449b56
lanyuechen <1252012805@qq.com>
2014-09-21 18:59:25 +0800

    算法更新，index中输入一个数独进行求解

--- sudoku.js
+++ sudoku.js
@@ -1,51 +1,95 @@
 var sudoku = (function(){
 	
-	var sudo = [
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0]
+	var sudo = {data : [], mark : []};
+	
+	var example = [
+		[0, 8, 0, 0, 0, 2, 7, 0, 0],
+		[5, 0, 3, 7, 0, 0, 4, 0, 0],
+		[0, 6, 7, 0, 5, 0, 0, 0, 3],
+		[6, 9, 0, 0, 7, 4, 0, 0, 5],
+		[0, 0, 5, 0, 0, 0, 1, 0, 0],
+		[2, 0, 0, 9, 1, 0, 0, 7, 4],
+		[3, 0, 0, 0, 2, 0, 8, 6, 0],
+		[0, 0, 2, 0, 0, 6, 5, 0, 7],
+		[0, 0, 6, 5, 0, 0, 0, 3, 0]
 	];
 
-	//填充数独
-	var fill = function(){
-		var count = 0;
-		for(var i = 0; i < 9; ){
-			for(var j = 0; j < 9; ){ 
-				count++;
-				var num = numFill(i, j);
-				if(!num){
-					sudo[i][j] = 0;
-					if(j == 0){
-						if(i == 0){
-							console.log('无解');
-							return false;
-						}
-						j = 8;
-						i--;
-					}else{
-						j--;
-					}
+	//获取数独状态
+	var getSudo = function(arr){
+		for(var i = 0; i < 9; i++){
+			sudo.data[i] = [];
+			sudo.mark[i] = [];
+			for(var j = 0; j < 9; j++){
+				if(arr[i][j] > 0){
+					sudo.data[i][j] = arr[i][j];
+					sudo.mark[i][j] = true;
 				}else{
-					sudo[i][j] = num;
-					j++;
+					sudo.data[i][j] = 0;
+					sudo.mark[i][j] = false;
 				}
 			}
-			i++;
+		}
+	};
+
+	//填充数独
+	var fill = function(arr){
+		arr = arr || example;
+		getSudo(arr);
+
+		var count = 0;										//填充次数
+		var pos = sudo.mark[0][0] ? next(0, 0) : {x : 0, y : 0};	//数独遍历起始位置
+		var num = false;										//可填充数字
+		while(pos){
+			count++;						//计数
+			num = numFill(pos.x, pos.y);
+			if(!num){
+				sudo.data[pos.x][pos.y] = 0;
+				pos = prev(pos.x, pos.y);
+			}else{
+				sudo.data[pos.x][pos.y] = num;
+				pos = next(pos.x, pos.y);
+			}
 		}
 		console.log('count='+count);
-		return sudo;
-	}
+		return sudo.data;
+	};
+
+	//下一个空格位置
+	var next = function(x, y){
+		for(var j = y + 1; j < 9; j++){
+			if(!sudo.mark[x][j]){
+				return{x : x, y : j};
+			}
+		}
+		for(var i = x + 1; i < 9; i++){
+			for(var j = 0; j < 9; j++){
+				if(!sudo.mark[i][j]){
+					return{x : i, y : j};
+				}
+			}
+		}
+	};
+
+	//回溯上一个空格位置
+	var prev = function(x, y){
+		for(var j = y - 1; j >=0; j--){
+			if(!sudo.mark[x][j]){
+				return {x : x, y : j};
+			}
+		}
+		for(var i = x - 1; i >= 0; i--){
+			for(var j = 8; j >=0; j--){
+				if(!sudo.mark[i][j]){
+					return {x : i, y : j};
+				}
+			}
+		}
+	};
 
 	//返回该位置可填的下一个数字
 	var numFill = function(x, y){
 		var arrNo = numValidNo(x, y) || [];
-		for(var i = sudo[x][y] + 1; i <= 9; i++){
+		for(var i = sudo.data[x][y] + 1; i <= 9; i++){
 			if(arrNo.indexOf(i) < 0){
 				return i;
 			}
@@ -58,19 +102,19 @@ var sudoku = (function(){
 		var arrNo = [];
 		for(var i = 0; i < 9; i++){
 			//获取元素所在行已填数字
-			if(arrNo.indexOf(sudo[x][i]) < 0 && i !== y){
-				arrNo.push(sudo[x][i]);
+			if(arrNo.indexOf(sudo.data[x][i]) < 0 && i !== y){
+				arrNo.push(sudo.data[x][i]);
 			}
 			//获取元素所在列已填数字
-			if(arrNo.indexOf(sudo[i][y]) < 0 && i !==x){
-				arrNo.push(sudo[i][y]);
+			if(arrNo.indexOf(sudo.data[i][y]) < 0 && i !==x){
+				arrNo.push(sudo.data[i][y]);
 			}
 		}
 		//获取元素所在块已填数字
 		for(var i = parseInt(x/3) * 3; i < parseInt(x/3) * 3 + 3; i++){
 			for(var j = parseInt(y/3) * 3; j < parseInt(y/3) * 3 + 3; j++){
-				if(arrNo.indexOf(sudo[i][j]) < 0 && i !== x && j !== y){
-					arrNo.push(sudo[i][j]);
+				if(arrNo.indexOf(sudo.data[i][j]) < 0 && i !== x && j !== y){
+					arrNo.push(sudo.data[i][j]);
 				}
 			}
 		}
